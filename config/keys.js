c9d059c3e1656246814d904af1607ef7dab64eda
sommysab <sommysabudeh@yahoo.com>
2019-09-07 18:52:13 +0100

    Version 1.0.0

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = { 
+    mongoURI: 'mongodb+srv://sommysab:bZ9afb48074jNikx@mern-shopping-didkn.mongodb.net/mern_shopping?retryWrites=true&w=majority'
+}
+
---
+++ routes/api/items.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+// Item Model
+const Item = require('../../models/Item');
+
+// @route  GET api/items
+// @desc   Get All Items
+// @access Public
+router.get('/', (req, res) => {  // only slash since we are already directed here
+    Item.find()
+      .sort({date: -1})
+      .then(items => res.json(items))
+}); 
+
+
+// @route  POST api/items
+// @desc   Creat ITEM
+// @access Public
+router.post('/', (req, res) => {  
+    // Create new object // Item relative to Model name
+    const newItem = new Item({  
+        name: req.body.name // note: date added automatically with model
+    }); 
+
+    // Save to db
+    newItem.save().then(item => res.json(item));
+}); 
+
+
+// @route  DELETE api/items
+// @desc   DELETE ITEM
+// @access Public
+router.delete('/:id', (req, res) => {  
+    Item.findById(req.params.id)
+      .then(item => item.remove().then(() => res.json({success: true})))
+      .catch(err => res.status(404).json({success: false}));
+});
+ 
+
+
+module.exports = router;
