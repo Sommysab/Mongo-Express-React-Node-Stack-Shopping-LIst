a26ccee40e8e34ae956328a9f8e7db8c8e522b76
sommysab <sommysabudeh@yahoo.com>
2019-09-07 23:39:32 +0100

    Version 1.0.0

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api/items', items);
 
 if(process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.stative('client/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.htom'));
   });
